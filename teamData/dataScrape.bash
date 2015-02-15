58787016f9ad3e58b9e5204721d65de791bde9fa
gg_hatano <adad0405@gmail.com>
2015-02-15 19:16:25 +0900

    create all team stats data

--- teamData/dataScrape.bash
+++ teamData/dataScrape.bash
@@ -5,12 +5,23 @@ dir=$(dirname $0)
 # teamName=$1
 teamName="giants"
 
-echo "TEAM,NAME,ATBAT,SINGLE,DOUBLE,TRIPLE,HOMERUN,BB,SO"
-cat giants.html | 
-grep "text-align:center" | 
-sed 's/<[^>]*>/,/g' | 
-sed 's/,,*/,/g' |
-sed -e 's/　//' -e 's/\./0./g' -e 's/^,//' -e 's/,$//' |
-awk -F"," -v OFS="," -v teamName=$teamName '{print teamName, $2, $6, $8-$9-$10-$11,$9,$10,$11, $18+$19+$20, $21}'| 
-grep -v "-" > $dir/giants.csv
+: > $dir/stats.dat
+
+for line in `cat $dir/url_data.dat` 
+do
+  teamName=$(echo $line | cut -d"," -f1)
+  echo $teamName
+  url=$(echo $line | cut -d"," -f2)
+  echo $url
+
+  curl $url | 
+  grep "text-align:center" | 
+  sed 's/<[^>]*>/,/g' | 
+  sed 's/,,*/,/g' |
+  sed -e 's/　//' -e 's/\./0./g' -e 's/^,//' -e 's/,$//' |
+  awk -F"," -v OFS="," -v teamName=$teamName '{print teamName, $2, $6, $8-$9-$10-$11,$9,$10,$11, $18+$19+$20, $21}'| 
+  grep -v "-" >> $dir/stats.dat
+done
+
+cat $dir/stats.dat
 
