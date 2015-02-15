88038d7eb984868bf1bc8c0595088a5387b29f5b
gg_hatano <adad0405@gmail.com>
2015-02-15 22:14:06 +0900

    simulation by 9 members

--- Simulation.java
+++ Simulation.java
@@ -1,16 +1,29 @@
 
 class Simulation{
   public static void main(String[] args) {
-    PlayerData playerData = new PlayerData("坂本勇人");
-    System.out.println(playerData.batterName);
 
-    GameSituation gameSituation = new GameSituation(playerData);
+    String[] startingMember = {"坂本勇人", "阿部慎之助", "村田修一", "長野久義", 
+                               "片岡治大", "アンダーソン", "セペダ", "亀井善行", "高橋由伸"};
+
+    /* definition */
+    PlayerData[] startingMemberData = new PlayerData[startingMember.length];
+    GameSituation[] gameSituation = new GameSituation[startingMember.length];
+
+    for(int i = 0; i < startingMemberData.length; i++){
+      startingMemberData[i] = new PlayerData(startingMember[i]);
+      gameSituation[i] = new GameSituation(startingMemberData[i]);
+    }
+
 
     /* simulation until one game ends */
-    while(GameSituation.inningGetter() <= 9){
-      gameSituation.attack();
-      /* System.out.println(GameSituation.outCountGetter()); */
+    int batterNumber = 0;
+    while (GameSituation.inningGetter()<10) {
+      gameSituation[batterNumber].attack();
+      batterNumber++;
+      batterNumber = batterNumber % 9;
     }
-    System.out.println("Score:" + GameSituation.scoreGetter());
+
+    /* output the result of game simulation */
+    System.out.println("Simulation Result: score is " + GameSituation.scoreGetter());
   }
 }
